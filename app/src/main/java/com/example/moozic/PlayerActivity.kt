59265a11063e37753a94c3f7c5abb849bad5439b
AmanV <a4amanver15@gmail.com>
2022-07-20 19:13:07 +0530

    Share Feature Added

--- app/src/main/java/com/example/moozic/PlayerActivity.kt
+++ app/src/main/java/com/example/moozic/PlayerActivity.kt
@@ -7,6 +7,7 @@ import android.content.ServiceConnection
 import android.graphics.Color
 import android.media.MediaPlayer
 import android.media.audiofx.AudioEffect
+import android.net.Uri
 import android.os.Bundle
 import android.os.IBinder
 import android.widget.LinearLayout
@@ -131,6 +132,14 @@ class PlayerActivity : AppCompatActivity(),ServiceConnection,MediaPlayer.OnCompl
 
             }
 
+        }
+        //share Btn
+        binding.shareBtnPA.setOnClickListener {
+            val shareIntent=Intent()
+            shareIntent.action=Intent.ACTION_SEND
+            shareIntent.type="audio/*"
+            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(musicListPA[songPosition].path))
+            startActivity(Intent.createChooser(shareIntent,"Sharing Music File!!"))
         }
 
      }
@@ -301,4 +310,6 @@ class PlayerActivity : AppCompatActivity(),ServiceConnection,MediaPlayer.OnCompl
             dialog.dismiss()
         }
     }
+
+
 }
